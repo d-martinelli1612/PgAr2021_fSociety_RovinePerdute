34361d5f3d9fc86ac02e426d140777dd521cbb4d
d-martinelli1612 <d.martinelli1612@gmail.com>
2021-05-26 09:22:30 +0200

    Migliorie alla creazione delle matrici

--- src/it/unibs/pa/rovinePerdute/Algoritmo.java
+++ src/it/unibs/pa/rovinePerdute/Algoritmo.java
@@ -1,156 +1,64 @@
 package it.unibs.pa.rovinePerdute;
 
 import java.util.ArrayList;
+import java.util.SortedMap;
 
 public  class  Algoritmo {
-    int matricePerscorsi[][];
+    int matricePercorsi[][];
     ArrayList<Citta> listaCitta;
 
     public Algoritmo(ArrayList listaCitta) {
         this.listaCitta = listaCitta;
-        this.matricePerscorsi = new int[listaCitta.size()][listaCitta.size()];
+        this.matricePercorsi = new int[listaCitta.size()][listaCitta.size()];
     }
 
     public void matriceDistanze() {
-
-
-        int distanza;
-
-        //aggiungere che se il cicli
-
         //seleziona righe matrice ciascuna corrispondente a una localita'
-        for (int i=0; i < listaCitta.size(); i++){
+        for (int i=0; i < this.listaCitta.size(); i++){
             //Controlla quante destinazioni sono raggiungibili partendo dalla localita' selezionata
-            for (int j=0; j < listaCitta.get(i).getLinkTo().size(); j++){
+            for (int j=0; j < this.listaCitta.get(i).getLinkTo().size(); j++){
                 //Imposta il percorso come "esistente" all'interno della matricePercorsi
-                matricePerscorsi[i][listaCitta.get(i).getLinkTo().get(j)] = 1;
+                this.matricePercorsi[i][this.listaCitta.get(i).getLinkTo().get(j)] = 1;
             }
-        }
-
-        // STAMPA
-        for (int k = 0; k < listaCitta.size(); k++){
-
-                for (int n = 0; n < listaCitta.size(); n++) {
-                    System.out.print(String.format(" %d\t ", matricePerscorsi[k][n]));
-
-                }
-            System.out.println();
         }
     }
 
 
     // matrice per trovare l'altitudine
-    public void trovaAltitudine ( ){
-          int matriceAltitudine [] [] = new int [ listaCitta.size()][listaCitta.size()];
-        //seleziona righe matrice ciascuna corrispondente a una localita'
-        for (int i=0; i < listaCitta.size(); i++){
-            //Controlla quante destinazioni sono raggiungibili partendo dalla localita' selezionata
-            for (int j=0; j < listaCitta.size(); j++){
+    public void percorsoAltitudine(Algoritmo Sorgente){
 
-                if (matricePerscorsi[i][j] == 1) {
-                    matriceAltitudine[i][j] = (Math.abs(listaCitta.get(i).getAltitudine()- listaCitta.get(j).getAltitudine()));
+        for (int i=0; i < Sorgente.listaCitta.size(); i++){
+
+            for (int j=0; j < Sorgente.listaCitta.size(); j++){
+                if (Sorgente.matricePercorsi[i][j] == 1) {
+                    this.matricePercorsi[i][j] = (Math.abs(Sorgente.listaCitta.get(i).getAltitudine()
+                            - Sorgente.listaCitta.get(j).getAltitudine()));
                 }else{
-                    matriceAltitudine[ i][ j ]= -1;
+                    this.matricePercorsi[i][j]= -1;
+                }
+            }
+        }
+    }
+
+
+    public void percorsoPlanare(Algoritmo Sorgente){
+        int distanza, indiceX, indiceY;
+
+        for (int i=0; i < Sorgente.listaCitta.size(); i++){
+            indiceX = Sorgente.listaCitta.get(i).getCoordinataX();
+            indiceY = Sorgente.listaCitta.get(i).getCoordinataY();
+            for (int j=0; j < Sorgente.listaCitta.size(); j++){
+
+                if (Sorgente.matricePercorsi[i][j] == 1) {
+                    distanza = (int) Math.sqrt(Math.pow((indiceX - listaCitta.get(j).getCoordinataX()), 2)
+                            + Math.pow((indiceY - listaCitta.get(j).getCoordinataY()), 2));
+                    this.matricePercorsi[i][j]= distanza;
+
+                }else{
+                    this.matricePercorsi[i][j]= -1;
                 }
 
             }
         }
-
-        for (int k = 0; k < listaCitta.size(); k++){
-
-            for (int n = 0; n < listaCitta.size(); n++) {
-                System.out.print(String.format(" %d \t ", matriceAltitudine[k][n]));
-
-            }
-            System.out.println();
-        }
-
     }
-
-
-    public void trovaDistanza ( ){
-        int distanza;
-        int matriceDistanza[] [] = new int [ listaCitta.size()][listaCitta.size()];
-
-        for (int i=0; i < listaCitta.size(); i++){
-            for (int j=0; j < listaCitta.size(); j++){
-
-                if (matricePerscorsi[i][j] == 1) {
-                    distanza = (int) Math.sqrt(Math.pow((listaCitta.get(i).getCoordinataX() - listaCitta.get(j).getCoordinataX()), 2)
-                            + Math.pow((listaCitta.get(i).getCoordinataY() - listaCitta.get(j).getCoordinataY()), 2));
-                    matriceDistanza[i][j]= distanza;
-
-                }else{
-                    matriceDistanza[i][j]= -1;
-                }
-
-            }
-        }
-        for (int k = 0; k < listaCitta.size(); k++){
-
-            for (int n = 0; n < listaCitta.size(); n++) {
-                System.out.print(String.format(" %d \t ", matriceDistanza[k][n]));
-
-            }
-            System.out.println();
-        }
-
-    }
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// CREAZIONE SBAGLIATA DELLA MATRICE
-/*for (int i = 0; i < citta.size(); i++){
-            for ( int j = 0 ; j < citta.size(); j++)
-                for (int h = 0; h < citta.get(j).getLinkTo().size(); h++) {
-
-                    if (citta.get(j).getLinkTo().get(h) == citta.get(i).getId()) {
-                        //distanza = radice di differenza di x di differenza di y
-                        distanza = Math.sqrt(Math.pow((citta.get(i).getCoordinataX() - citta.get(j).getCoordinataX()), 2)
-                                + Math.pow((citta.get(i).getCoordinataY() - citta.get(j).getCoordinataY()), 2));
-
-                        matrice[i][j] = distanza;
-
-                    } else {
-                        matrice[i][j] = -1;
-                    }
-                }
-
-        }*/
--- src/it/unibs/pa/rovinePerdute/Main.java
+++ src/it/unibs/pa/rovinePerdute/Main.java
@@ -10,31 +10,37 @@ public class Main {
     public static void main(String args[]) throws XMLStreamException {
 
         ArrayList<Citta> listaCitta = InputXML.leggiInputCitta();
-        Algoritmo al = new Algoritmo(listaCitta);
-
-
-
-
-
+        Algoritmo mappaSorgente = new Algoritmo(listaCitta);
+        Algoritmo mappaAltitudine = new Algoritmo(listaCitta);
+        Algoritmo mappaCartesiana = new Algoritmo(listaCitta);
 
         // Stampa lista citta
-        for (int i=0; i<listaCitta.size(); i++){
+        /*for (int i=0; i<listaCitta.size(); i++){
             listaCitta.get(i).stampaCitta();
-        }
+        }*/
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        mappaSorgente.matriceDistanze();
 
-        al.matriceDistanze();
-        System.out.println();
-        // trova altitudine
-        al.trovaAltitudine();
-        // trova la distanza
-        System.out.println();
-        al.trovaDistanza();
+        //Trova percorso altitudine
+        mappaAltitudine.percorsoAltitudine(mappaSorgente);
+        //STAMPA
+        /*for (int i=0; i<mappaAltitudine.listaCitta.size(); i++){
+            for (int j=0; j<mappaAltitudine.listaCitta.size(); j++){
+                System.out.print(String.format("|%5d|", mappaAltitudine.matricePercorsi[i][j]));
+            }
+            System.out.println();
+        }*/
 
+        //System.out.println();
+
+        //Trova percorso distanza planare
+        mappaCartesiana.percorsoPlanare(mappaSorgente);
+        //STAMPA
+        /*for (int i=0; i<mappaCartesiana.listaCitta.size(); i++){
+            for (int j=0; j<mappaCartesiana.listaCitta.size(); j++){
+                System.out.print(String.format("|%5d|", mappaCartesiana.matricePercorsi[i][j]));
+            }
+            System.out.println();
+        }*/
     }
 }
